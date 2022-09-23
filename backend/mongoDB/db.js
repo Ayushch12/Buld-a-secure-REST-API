a6394bf7c17009d89e04610fae89ca70eea9f66d
Ayush chalisee <ayushchalise12@gmail.com>
2022-09-24 01:42:23 +0200

    first commit

---
+++ backend/mongoDB/db.js
@@ -0,0 +1,23 @@
+//Importation of Mongoose:
+const mongoose = require('mongoose');
+
+//Importation of dotenv for environment variables :
+const dotenv = require("dotenv").config();
+
+
+//Conection de mongoDB à l'API grâce à mongoose :
+
+  mongoose.connect("mongodb+srv://AYUSHJOLLY:v0A6k3idM3TENquD@jollyayush.qnnedfk.mongodb.net/?retryWrites=true&w=majority")
+  .then(()=>{
+  console.log("successfully connection with mongoose");
+  })
+  
+  .catch((error)=>{
+  console.log("Unable to connect to mongoDB Atlas");
+  console.error(error);
+  
+  });
+
+
+//Exportation mangoose :    
+module.exports = mongoose;
